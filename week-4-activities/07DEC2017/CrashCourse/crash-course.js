6064d4be4812806a829c03aafd4db374b47878c5
Phil S <philipstubbs13@gmail.com>
2017-12-29 20:48:32 -0600

    reviewing javascript and jquery activities

---
+++ week-4-activities/07DEC2017/CrashCourse/crash-course.js
@@ -0,0 +1,100 @@
+// CRASH COURSE JS
+// ==========================================================
+
+// 1. BASIC VARIABLES
+// ==========================================================
+
+// Create a basic variable
+var basicVariable = 0;
+
+
+
+
+// 2. ARRAYS
+// ==========================================================
+
+// Create an array of five strings
+var myArray = ["stringOne", "stringTwo", "stringThree", "stringFour", "stringFive"];
+
+
+
+
+// Create an array of five numbers
+var numberArray = [1, 2, 3, 4, 5];
+
+
+
+
+// 3. FOR LOOPS
+// ==========================================================
+
+// Create a for loop that loops through and prints "My name is Bob five times"
+for (var i = 0; i < 5; i++) {
+	console.log("My name is Bob five times");
+}
+
+
+
+// Create a for loop that loops through your five string array
+for (var i = 0; i < numberArray.length; i++) {
+	console.log(numberArray[i]);
+}
+
+
+
+
+// 4. FUNCTIONS
+// ==========================================================
+
+// Create a function that takes two numbers and divides the first number by the second.
+// Then call that function
+var divide = function(x, y) {
+var result = y/x;
+console.log(result);
+return result;
+}
+
+divide(2, 6);
+
+
+
+
+// Create a function that takes in an array of numbers and then loops through the array and prints out numbers.
+// Then call that function
+
+
+
+
+// 5. OBJECTS
+// ==========================================================
+
+// Create a JavaScript Object
+
+
+
+
+// Console log any three of the properties in that object
+
+
+
+// Create an Array of 5 Objects
+
+
+
+// Console log 3 properties for every one of the five objects
+
+
+
+// 6. JQUERY EVENTS
+// ==========================================================
+
+// Create a basic html button then create an onClick event that triggers an on click event.
+
+
+
+
+// 7. OVERALL STRUCTURE
+// ==========================================================
+
+// Talk to student a little about the approach for "getting started" on an application.
+// Talk about the concept of variables, functions, calls.
